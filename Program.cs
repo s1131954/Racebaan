475359db0367f7d9b0e8515b92c1b75d9d8723b9
s1131954 <43470059+s1131954@users.noreply.github.com>
2021-01-21 16:40:53 +0100

    Opzet

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Racebaan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
